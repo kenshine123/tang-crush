6723628b4500dd3a939c22fb12cbebebd3d9ce0f
kenshine123 <86759542+kenshine123@users.noreply.github.com>
2021-07-01 16:49:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,16 @@
 const CONFIG = {
-    introTitle: 'Ốc à!',
-    introDesc: `Anh đọc hết mười vạn câu hỏi vì sao 
-    nhưng vẫn không sao hiểu được 
-    anh thích em nhiều thế ;) `,
+    introTitle: 'Silk à!',
+    introDesc: `Anh tuy không phải là chiếc lò vi sóng, nhưng anh đủ sức để hâm nóng trái tim em ;) `,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'yêu Nàng Thơ của a nhiều lắm.',
+    reply: 'em có ghét anh đâu, em thích a còn không hết',
+    mess: 'Anh biết mà 🥰. trêu em vậy thôi 😘😘',
+    messDesc: 'chiều nay nhớ đi lễ nhé....',
     btnAccept: 'Okiiii luôn <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
